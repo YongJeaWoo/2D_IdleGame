154c275bde6e11dbf5c912ec1cea6dc53a3f336b
sewonnoh <shtpdnjs3658@gmail.com>
2024-08-21 17:46:30 +0900

    캐릭터 및 몬스터 체력 구현
    - 캐릭터 및 몬스터 BigInteger 자료형으로 체력 구현
    - 몬스터 피격 구현, 몬스터 사망은 현재 Destroy로 구현
    - 캐릭터 피격은 구현 안되어있고, 검 프리팹 내부 KnifeAttack의 attackPoint (공격력)부분 차등 부여 및 몬스터 체력 차등 부여 필요

--- Assets/Scripts/Knife/KnifeAttack.cs
+++ Assets/Scripts/Knife/KnifeAttack.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class KnifeAttack : MonoBehaviour
 {
+    [SerializeField] private string attackPointString;
+    private BigInteger attackPoint;
     private ObjectPool pool;
+
+    private void Start()
+    {
+        attackPoint = BigInteger.Parse(attackPointString);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Enemy"))
         {
+            var health = collision.GetComponent<BaseHealth>();
+            health.Hit(attackPoint);
             Release();
         }
     }
