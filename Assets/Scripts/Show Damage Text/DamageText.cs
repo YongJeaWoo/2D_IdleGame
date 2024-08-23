57ff1bc025d2cd0473149febfa26fd78b644aa90
sewonnoh <shtpdnjs3658@gmail.com>
2024-08-23 20:52:14 +0900

    데미지 표시 텍스트 단위 적용 작업
    - 데미지 표시 Text를 띄울 때, 단위가 너무 높아지면 한국식으로 단위를 표기하는 단위 적용을 구현

--- Assets/Scripts/Show Damage Text/DamageText.cs
+++ Assets/Scripts/Show Damage Text/DamageText.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.Numerics;
 using TMPro;
 using UnityEngine;
@@ -16,7 +17,7 @@ public class DamageText : MonoBehaviour
 
     public void ShowText(BigInteger damage)
     {
-        dmgText.text = damage.ToString();
+        dmgText.text = SetText(damage);
         StartCoroutine(TextCoroutine());
     }
 
@@ -29,10 +30,31 @@ public class DamageText : MonoBehaviour
             timer += Time.deltaTime;
 
             transform.Translate(UnityEngine.Vector3.up * upSpeed * Time.deltaTime);
-
+            
             yield return null;
         }
-
+        
         Destroy(gameObject);
+    }
+
+    private string SetText(BigInteger value)
+    {
+        string[] units = { "", "만", "억", "조", "경" };
+        List<string> parts = new List<string>();
+        int unitIndex = 0;
+
+        while (value > 0 && unitIndex < units.Length)
+        {
+            BigInteger currentValue = value % 10000;
+
+            if (currentValue > 0)
+            {
+                parts.Insert(0, $"{currentValue}{units[unitIndex]}");
+            }
+            value /= 10000;
+            unitIndex++;
+        }
+
+        return string.Join("", parts);
     }
 }
