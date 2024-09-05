863054ae6083553f3d66363722119b726676f478
YongJeaWoo <subrosastar97@gmail.com>
2024-09-05 14:30:15 +0900

    [feat] UI 업데이트
    모작 게임의 UI 업데이트로 UI 전체적인 작업
    
    [refactor] 변경된 UI에 맞게 일부 코드 리팩토링

--- Assets/Scripts/Buttons/Effect Button/ClickEffectButton.cs
+++ Assets/Scripts/Buttons/Effect Button/ClickEffectButton.cs
@@ -3,23 +3,55 @@ using UnityEngine.UI;
 
 public class ClickEffectButton : MonoBehaviour
 {
-    [Header("선택된 색상")]
-    [SerializeField] private Color selectColor;
+    [Header("선택 되었을 때 해제 표시할 이팩트")]
+    [SerializeField] private Sprite changeImage;
+    [Header("선택 되었을 때 표시할 오브젝트")]
+    [SerializeField] private GameObject changeObject;
+    [Header("해제 되었을 때 표시할 오브젝트")]
+    [SerializeField] private GameObject originObject;
 
-    [Header("선택되지 않은 색상")]
-    [SerializeField] private Color deSelectColor;
+    [Header("선택 되었을 때 색상")]
+    [SerializeField] private Color selectedColor;
+    [Header("해제 되었을 때 색상")]
+    [SerializeField] private Color deselectedColor;
+
+    private Image spriteImage;
+
+    private bool isSelect = true;
+
+    private void Awake()
+    {
+        InitChangeObjectComponent();
+    }
+
+    private void InitChangeObjectComponent()
+    {
+        spriteImage = changeObject.GetComponentInChildren<Image>();
+    }
 
     public void SelectButton(Button button)
     {
-        ColorBlock buttonColor = button.colors;
-        buttonColor.selectedColor = selectColor;
-        button.colors = buttonColor;
+        originObject.SetActive(!isSelect);
+        changeObject.SetActive(isSelect);
+        spriteImage.sprite = changeImage;
+
+        SetButton(button, selectedColor);
     }
 
     public void DeSelectButton(Button button)
     {
-        ColorBlock buttonColor = button.colors;
-        buttonColor.selectedColor = deSelectColor;
-        button.colors = buttonColor;
+        originObject.SetActive(isSelect);
+        changeObject.SetActive(!isSelect);
+
+        SetButton(button, deselectedColor);
+    }
+
+    private void SetButton(Button button, Color color)
+    {
+        ColorBlock buttonColors = button.colors;
+        buttonColors.normalColor = color;
+        buttonColors.highlightedColor = color;
+        buttonColors.selectedColor = color;
+        button.colors = buttonColors;
     }
 }
